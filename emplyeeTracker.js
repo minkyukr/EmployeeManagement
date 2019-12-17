d60e8812dfef06199e15d8f9421a704a23a43e09
Michel Kong <minkya_u@daum.net>
2019-12-17 13:22:53 -0800

    asfsaf

---
+++ emplyeeTracker.js
@@ -0,0 +1,125 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Minkyukr91!",
+  database: "employee_trackerDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runTracker();
+});
+
+function runTracker() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "Add departments, roles, employees",
+        "View departments, roles, employees",
+        "Update employee roles"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "Add departments, roles, employees":
+        addDRE();
+        break;
+
+      case "View departments, roles, employees":
+        viewDRE();
+        break;
+
+      case "Update employee roles":
+        updateRoles();
+        break;
+      }
+    });
+}
+// until here, the user can choose what it would like to do.
+
+function addDRE() {
+  inquirer
+    .prompt({
+      name: "artist",
+      type: "input",
+      message: "What artist would you like to search for?"
+    })
+    .then(function(answer) {
+      var query = "SELECT position, song, year FROM top5000 WHERE ?";
+      connection.query(query, { artist: answer.artist }, function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+          console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+        }
+        runTracker();
+      });
+    });
+}
+
+function viewDRE() {
+  var query = "SELECT artist FROM top5000 GROUP BY artist HAVING count(*) > 1";
+  connection.query(query, function(err, res) {
+    for (var i = 0; i < res.length; i++) {
+      console.log(res[i].artist);
+    }
+    runTracker();
+  });
+}
+
+function updateRoles() {
+  inquirer
+    .prompt([
+      {
+        name: "start",
+        type: "input",
+        message: "Enter starting position: ",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      },
+      {
+        name: "end",
+        type: "input",
+        message: "Enter ending position: ",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ])
+    .then(function(answer) {
+      var query = "SELECT position,song,artist,year FROM top5000 WHERE position BETWEEN ? AND ?";
+      connection.query(query, [answer.start, answer.end], function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+          console.log(
+            "Position: " +
+              res[i].position +
+              " || Song: " +
+              res[i].song +
+              " || Artist: " +
+              res[i].artist +
+              " || Year: " +
+              res[i].year
+          );
+        }
+        runTracker();
+      });
+    });
+}
